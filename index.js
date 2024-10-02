f3bd516de4691d58a4bb9f4e62e2fb819359aafa
ballpanuwat25 <ballpanuwat25@gmail.com>
2024-10-02 23:13:49 +0700

    connect_db

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors())
 
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'tutorial'
+});
+
+// ลอง query ข้อมูลจากตาราง users
+app.get('/get-users', async (req, res) => {
+  try {
+    // Query ข้อมูลจากฐานข้อมูล
+    const [results] = await db.query('SELECT * FROM users');
+    console.log('Data from the database:', results);
+    
+    // ส่งข้อมูลไปให้ client
+    res.send(results);
+  } catch (err) {
+    console.error('Error querying the database:', err.message);
+    res.status(500).send('Error retrieving data from the database');
+  }
+});
+
 //Root equation
 app.post('/graphical', (req, res) => {
   //graphical method
